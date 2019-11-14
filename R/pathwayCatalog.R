5e2a90ecb67ff999cd4b80d77c7ea8e49d34f105
Richard Judson <judson.richard@epa.gov>
2019-11-14 07:10:33 -0500

    add CMAP pathways

--- R/pathwayCatalog.R
+++ R/pathwayCatalog.R
@@ -1,5 +1,5 @@
 #--------------------------------------------------------------------------------------
-#' Create a caalog of all of the possible pathways and wruite this to a
+#' Create a catalog of all of the possible pathways and wruite this to a
 #' file that can be used to hand select specific pathways to use
 #'
 #' @return No output.
@@ -10,13 +10,15 @@ pathwayCatalog = function(){
   load("../input/processed_pathway_data/msigdb_PATHWAYS.RData")
   load("../input/processed_pathway_data/RYAN_PATHWAYS.RData")
   load("../input/processed_pathway_data/bioplanet_PATHWAYS.RData")
+  load("../input/processed_pathway_data/CMAP_PATHWAYS.RData")
 
-  all_pathways <- rbind(bioplanet_PATHWAYS,msigdb_PATHWAYS,RYAN_PATHWAYS)
-  all_pathways <- all_pathways[,1:4]
-  all_pathways$superclass <- "-"
+  all_pathways <- rbind(bioplanet_PATHWAYS,msigdb_PATHWAYS,RYAN_PATHWAYS,CMAP_PATHWAYS)
+  all_pathways <- all_pathways[,c("pathway","pathset","pathway_class","path_info","ngene")]
+  all_pathways$super_class <- "-"
   all_pathways$useme <- 0
 
-  all_pathways <- all_pathways[c("pathway","useme","superclass","pathset","pathway_class","path_info")]
+  all_pathways <- all_pathways[c("pathway","useme","super_class","pathset","pathway_class","path_info","ngene")]
+  all_pathways <- unique(all_pathways)
   file <- "../input/processed_pathway_data/pathway_catalog.xlsx"
   write.xlsx(all_pathways,file)
 }
