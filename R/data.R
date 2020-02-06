ca0e5e9dc36ef7722fba5a76a4e7529a020b9a68
Judson <Judson.Richard@epa.gov>
2020-02-06 16:05:27 -0500

    updated

--- R/data.R
+++ R/data.R
@@ -6,7 +6,7 @@
 #'   sample_id (unique identifier for each sample), conc (concentration), time
 #'   (length of the experiment in hours), casrn, name (chemical name),
 #'   dtxsid.
-"CHEM_DICT"
+#"CHEM_DICT"
 
 #' Endocrine Pathways Example
 #'
@@ -14,14 +14,14 @@
 #'
 #' @format A data frame with 44 rows and 8 variables: pathset, pathway, ngene,
 #' ngene_in_httr, pathway_class, pathway_super_class, url, gene_list.
-"ENDOCRINE_PATHWAYS"
+#"ENDOCRINE_PATHWAYS"
 
 #' Fold Change Matrix Example
 #'
 #' Fold change matrix for 10 randomly selected chemicals from the phase 1 screen.
 #'
 #' @format A sample by gene matrix with 80 rows and 10,341 columns.
-"FCMAT2"
+#"FCMAT2"
 
 #' Endocrine Pathway Data
 #'
@@ -29,4 +29,4 @@
 #'
 #' @format A named list containing 44 elements. Each elements corresponds to a
 #'   pathway and consists of a vector of gene names.
-"pathway_data"
+#"pathway_data"
