341c1b3e29f819ada6cd8c5d002b07c0d0141da4
Judson <Judson.Richard@epa.gov>
2020-02-13 10:56:15 -0500

    change from pathways to sigantures

--- R/signatureTestMerging.R
+++ R/signatureTestMerging.R
@@ -9,6 +9,13 @@ signatureTestMerging <- function(to.file=F,
                         method="mygsea",
                         cname="Fulvestrant") {
   printCurrentFunction()
+
+  if(to.file) {
+    fname <- paste0("../output/signature_score_summary/signatureTestMerging",dataset,"_",sigset," ",method," ",cname,".pdf")
+    pdf(file=fname,width=8,height=10,pointsize=12,bg="white",paper="letter",pagecentre=T)
+  }
+  par(mfrow=c(3,2),mar=c(4,4,2,2))
+
   file <- paste0("../output/signature_score_summary/signaturescoremat_",sigset,"_",dataset,"_",method,".RData")
   print(file)
   load(file=file)
@@ -33,16 +40,18 @@ signatureTestMerging <- function(to.file=F,
 
       x <- smat1both$conc
       y <- smat1both$signature_score
-      plot(y~x,xlab="Conc",log="x",xlim=c(0.01,100),ylim=c(-1,1),main=paste(cname,"\n",signature),cex.lab=1.2,cex.axis=1.2)
+      plot(y~x,xlab="Conc",log="x",xlim=c(0.01,100),ylim=c(-1,1),main=paste(cname,"\n",signature),
+           cex.lab=1.2,cex.axis=1.2,ylab="MyGSEA Score",pch=21,bg="black",cex=1.5)
       lines(c(1e-5,1e5),c(0,0))
-      x <- smat0up$conc*1.1
+      x <- smat0up$conc#*1.1
       y <- smat0up$signature_score
       points(y~x,pch=24,bg="green")
-      x <- smat0dn$conc*0.9
+      x <- smat0dn$conc#*0.9
       y <- smat0dn$signature_score
       points(y~x,pch=25,bg="red")
       if(!to.file) browser()
     }
   }
+  if(to.file) dev.off()
 }
 
