2b57b69c50b88ad068196c7e9182f0da1895c46a
Richard Judson <rjudson@v2626umcth039.rtord.epa.gov>
2021-06-09 14:14:15 -0400

    remove debug statement

--- R/superTargetStats.R
+++ R/superTargetStats.R
@@ -28,7 +28,6 @@ superTargetStats <- function(do.load=F,
     RES <<- res.all
   }
   mat = RES
-  browser()
   mat[mat$count<2,"active"] = 0
   mat1 = mat[mat$active==1,]
   file = paste0("../output/super_target_boxplot/",celltype,"/super_target_boxplot_",celltype,"_",dataset,"_",sigset,"_",method,"_",hccut,"_",tccut,"_active.xlsx")
